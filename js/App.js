fdd80437e3c1f55039df0322bc779340a6e73abb
Abhishek Chopra <abhishek.chopra1796@gmail.com>
2016-10-28 11:41:02 +0530

    Fix#morn

--- js/App.js
+++ js/App.js
@@ -3,17 +3,6 @@ if (!window.location.origin) {
 }
 
 function init() {
-	
-	// Change all href links that go out of the origin to open in a new window
-//	$("a").not("[href*='" + window.location.origin + "']").attr("target", "new");
-	
-	var elements = document.querySelectorAll("a");
-	Array.prototype.forEach.call(elements, function(el, i){
-		  var a = new RegExp("/" + window.location.host + "/");
-		  if (!a.test(el.href))	el.target="new";
-	});
-
-
 	// Highlight all on load if highlighting
 	if (typeof hljs !== "undefined")
 		hljs.initHighlightingOnLoad();
